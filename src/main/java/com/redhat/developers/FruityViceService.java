f453ec619e7427ffa8f81abba34e90d4cf934d25
Sebastien LALLEMAND <sld@redhat.com>
2023-02-23 11:02:21 +0100

    fault tolerance

--- src/main/java/com/redhat/developers/FruityViceService.java
+++ src/main/java/com/redhat/developers/FruityViceService.java
@@ -6,6 +6,12 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.redhat.developers.FruityVice.Nutritions;
+
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/api/fruit")
@@ -15,6 +21,18 @@ public interface FruityViceService {
     @GET
     @Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 3, delay = 2000)
+    @Fallback(FruityViceFallback.class)
     FruityVice getFruitByName(@PathParam("name") String name);
+
+    public static class FruityViceFallback implements FallbackHandler<FruityVice> {
+
+        private static final FruityVice EMPTY_FRUITY_VICE = FruityVice.of("empty", Nutritions.of(0.0, 0.0));
+        @Override
+        public FruityVice handle(ExecutionContext context) {
+            return EMPTY_FRUITY_VICE;
+        }
+
+    }
 
 }
